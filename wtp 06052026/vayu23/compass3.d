5119eef2cbdb9aa18283b53738c20f9f3a2d8766
shaneeelliott <54601460+shaneeelliott@users.noreply.github.com>
2026-05-08 09:10:05 +0100

    added 0605 backup

---
+++ wtp 06052026/vayu23/compass3.d
@@ -0,0 +1,11 @@
+Channel 1 
+Device 0
+
+Com nmea2k
+usegyro off
+
+heading3 1 83 heading3.cal  heading3.fil  null.alm
+heel3    1 84 heel3.cal     heel3.fil	null.alm
+trim3    1 85 trim3.cal     trim3.fil	null.alm
+====================================================
+PRECISION 9
